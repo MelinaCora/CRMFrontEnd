367234198752f83fc0932dc419fdd3165042346b
MelinaCora <melinaccora97@gmail.com>
2024-11-28 14:12:28 -0300

    muestra 3 filas 4 columnas

--- static/js/views/index.js
+++ static/js/views/index.js
@@ -6,7 +6,7 @@ import { getCampaignTypes } from "../services/campaignTypeService.js";
 import { showSpinner, hideSpinner } from "../components/spinners.js";
 
 let currentPage = 0;
-const pageSize = 9;
+const pageSize = 12;
 
 const loadFilters = async () => {
     try {
